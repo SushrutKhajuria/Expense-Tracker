4b7eef10ff70caa93f6360c19ad23a56e69f6226
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-05-04 00:03:07 +0530

    FORGOT PASSWORD WORDFLOW

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { auth } from '../../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
